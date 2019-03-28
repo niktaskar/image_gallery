3562cbdd08780c7b71a910766aa3fb55d46e8bdd
Nikash Taskar <nikashtaskar@wustl.edu>
2019-03-28 13:46:25 -0500

    Finished Assignment
    12 pictures in 3 by 4 arrangement, hover changes grayscale to 0%, clicks cause image to overlay and be a large display, and everything in the background is made opaque.

--- main.js
+++ main.js
@@ -13,26 +13,31 @@ function handleClick(){
     bigDiv.innerHTML = "";
 
     var image = document.createElement("img");
-    image.className = "clicked-image"
+    image.className = "clicked-image";
+    image.id = event.target.id;
     image.src = imgArray[event.target.id];
-
-    var next = document.createElement("button")
-    next.innerText = "Next";
-    next.addEventListener("click", handleNext, false);
 
     var prev = document.createElement("button")
     prev.innerText = "Previous";
+    prev.id = "prev";
     prev.addEventListener("click", handlePrev, false);
+
+    var next = document.createElement("button")
+    next.innerText = "Next";
+    next.id = "next";
+    next.addEventListener("click", handleNext, false);
 
     var close = document.createElement("button")
     close.innerText = "Close";
+    close.id = "close";
     close.addEventListener("click", handleClose, false);
 
     bigDiv.append(image);
-    bigDiv.append(next);
     bigDiv.append(prev);
+    bigDiv.append(next);
     bigDiv.append(close);
 
+    document.getElementById("content").style.opacity ="0.25";
 }
 
 function handleOver(){
@@ -44,15 +49,70 @@ function handleOut(){
 }
 
 function handlePrev(){
-    var index = (event.target.id);
-    console.log(index);
+    var index = (parseInt(document.getElementsByClassName("clicked-image")[0].id) - 1 + 12)%12;
+
+    var bigDiv = document.getElementById("img-big")
+    bigDiv.innerHTML = "";
+
+    var image = document.createElement("img");
+    image.className = "clicked-image";
+    image.id = index;
+    image.src = imgArray[index];
+
+    var prev = document.createElement("button")
+    prev.innerText = "Previous";
+    prev.id = "prev";
+    prev.addEventListener("click", handlePrev, false);
+
+    var next = document.createElement("button")
+    next.innerText = "Next";
+    next.id = "next";
+    next.addEventListener("click", handleNext, false);
+
+    var close = document.createElement("button")
+    close.innerText = "Close";
+    close.id = "close";
+    close.addEventListener("click", handleClose, false);
+
+    bigDiv.append(image);
+    bigDiv.append(prev);
+    bigDiv.append(next);
+    bigDiv.append(close);
 }
 
 function handleNext(){
-    var index = event.target.id;
-    console.log(index);
+    var index = (parseInt(document.getElementsByClassName("clicked-image")[0].id) - 1 + 12)%12;
+
+    var bigDiv = document.getElementById("img-big")
+    bigDiv.innerHTML = "";
+
+    var image = document.createElement("img");
+    image.className = "clicked-image";
+    image.id = index;
+    image.src = imgArray[index];
+
+    var prev = document.createElement("button")
+    prev.innerText = "Previous";
+    prev.id = "prev";
+    prev.addEventListener("click", handlePrev, false);
+
+    var next = document.createElement("button")
+    next.innerText = "Next";
+    next.id = "next";
+    next.addEventListener("click", handleNext, false);
+
+    var close = document.createElement("button")
+    close.innerText = "Close";
+    close.id = "close";
+    close.addEventListener("click", handleClose, false);
+
+    bigDiv.append(image);
+    bigDiv.append(prev);
+    bigDiv.append(next);
+    bigDiv.append(close);
 }
 
 function handleClose(){
     document.getElementById("img-big").innerHTML = "";
+    document.getElementById("content").style.opacity ="1.0";
 }
